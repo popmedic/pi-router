8c58dcc8f0263055c941389182e7311f036edab2
Pop Medic <popmedic@gmail.com>
2022-01-16 15:19:03 -0700

    update to include init.d script

--- scripts/isc-server.sh
+++ scripts/isc-server.sh
@@ -5,6 +5,10 @@ sudo systemctl disable isc-dhcp-server
 sudo mv /etc/dhcp /etc/dhcp.backup
 sudo mv /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.backup
 sudo mv /etc/default/isc-dhcp-server6 /etc/default/isc-dhcp-server6.backup
+sudo mv /etc/init.d/isc-dhcp-server /etc/init.d/isc-dhcp-server.backup
+sudo mv /etc/init.d/isc-dhcp-server6 /etc/init.d/isc-dhcp-server6.backup
 sudo cp -r etc/dhcp /etc/
 sudo cp default/isc-dhcp-server /etc/default/isc-dhcp-server
 sudo cp default/isc-dhcp-server6 /etc/default/isc-dhcp-server6
+sudo cp etc/init.d/isc-dhcp-server /etc/init.d/isc-dhcp-server
+sudo cp etc/init.d/isc-dhcp-server6 /etc/init.d/isc-dhcp-server6
